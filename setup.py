5926eca476a8b2cce31c4ff2ee388f74a1e6a19c
Pete Schwamb <pete@schwamb.net>
2017-03-27 00:50:08 -0500

    Add pytz to requirements

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests', 'python-dateutil'],
+    install_requires=['requests', 'python-dateutil', 'pytz'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
